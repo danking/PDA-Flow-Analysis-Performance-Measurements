799fb0739561d21b2c84b8c8c5e92a870ed82c87
Daniel King <danking@ccs.neu.edu>
2012-05-11 15:08:21 -0400

    switch to pda-risc-enhanced and properly handle the register env
     - labeled code points should capture the state of the register environment at
       the label insn and replace that when they are called

--- run-cfa2.rkt
+++ run-cfa2.rkt
@@ -1,16 +1,16 @@
 #lang racket
 (require "../cfa2/cfa2.rkt"
-         "../pda-to-pda-risc/pdarisc-node-graph.rkt"
-         "../pda-to-pda-risc/node-graph.rkt"
          (only-in "../cfa2/utilities.rkt" bpset->fv-hash)
          (only-in "../racket-utils/similar-sets.rkt" get-basic-set)
-         (rename-in "../pda-to-pda-risc/pdarisc-data.rkt"
+         (rename-in "../pda-to-pda-risc/risc-enhanced/data.rkt"
                     (assign assign-node)
                     (assign? assign-node?)
                     (push push-node)
                     (push? push-node?)
                     (pop pop-var-rhs)
-                    (pop? pop-var-rhs?)))
+                    (pop? pop-var-rhs?)
+                    (pda-term-succs term-succs)
+                    (pda-term-insn  term->node)))
 
 (provide run-cfa2)
 
@@ -20,11 +20,7 @@
      (begin (displayln name)
             (time expr)))))
 
-(define (run-cfa2 pda-risc #:debug [debug 0] #:max-stack-height [max-stack-height +inf.0])
-  (define node-graph (time/named
-                      "building node-graph"
-                      (build-node-graph pda-risc)))
-
+(define (run-cfa2 pda-risc-enh #:debug [debug 0] #:max-stack-height [max-stack-height +inf.0])
   ;; an AValue is a [SetOf Value]
   ;; a RegisterEnv is a [Hash Symbol AValue]
   (define empty-register-env (hash))
@@ -40,38 +36,33 @@
 
 
   ;; Min Headroom
-  (define (min-headroom node-graph)
+  (define (min-headroom)
+    ;; label-closures : LabelName -> RegisterEnv
+    (define label-closures (make-hasheq))
+
     ;; env-update : RegisterEnv RegisterName [U Value [SetOf Value]] -> RegisterEnv
     (define (env-update env reg val)
       (let ((val (if (set? val) val (set val))))
         (match reg
-          ((named-reg name)
-           (hash-set env (syntax-e name) val))
-          ((nameless-reg)
-           (hash-set env 'nameless-reg val)))))
+          ((register _ uid _ _)
+           (hash-set env uid val)))))
 
     ;; env-ref : RegisterEnv RegisterName -> [SetOf Value]
     (define (env-ref env reg)
       (match reg
-        ((named-reg name)
+        ((register name uid _ _)
          (hash-ref env
-                   (syntax-e name)
+                   uid
                    (lambda () (begin (printf "the register, ~a, is not bound yet in ~a. Returning 'bottom.\n" (syntax-e name) env)
-                                     'bottom))))
-        ((nameless-reg)
-         (hash-ref env 'nameless-reg))))
+                                     'bottom))))))
 
     ;; eval-pure-rhs : pure-rhs RegisterEnv -> [SetOf Value]
     (define (eval-pure-rhs pure-rhs env)
       (match pure-rhs
-        ((or (named-reg _)
-             (nameless-reg))
+        ((register _ _ _ _)
          (env-ref env pure-rhs))
         (_ (set pure-rhs))))
 
-    ;; push-node?/uid : Key -> Boolean
-    (define (push-node?/uid uid)
-      (push-node? (uid->node uid)))
     ;; pop-node? : Node -> Boolean
     (define (pop-node? node)
       (match node
@@ -85,30 +76,22 @@
          reg)
         (_ (error 'pop-node-reg "wasn't given a 'pop' node (an assign node)"))))
 
-    ;; pop-node?/uid : Key -> Boolean
-    (define (pop-node?/uid uid)
-      (pop-node? (uid->node uid)))
-
-    ;; uid->node : Key -> Node
-    (define (uid->node uid)
-      (node-graph-get-node node-graph uid))
-
     ;; push-state? : FlowState -> Boolean
     (define (push-state? flow-state)
-      (push-node?/uid (abstract-state-node (flow-state-astate flow-state))))
+      (push-node? (abstract-state-node (flow-state-astate flow-state))))
     ;; pop-state? : FlowState -> Boolean
     (define (pop-state? flow-state)
-      (pop-node?/uid (abstract-state-node (flow-state-astate flow-state))))
+      (pop-node? (abstract-state-node (flow-state-astate flow-state))))
 
     ;; astate-similar? : AState AState -> Boolean
     (define astate-similar? (match-lambda*
-                             [(list (abstract-state node1 re1 st1)
-                                    (abstract-state node2 re2 st2))
-                              (and (equal? node1 node2)
+                             [(list (abstract-state term1 re1 st1)
+                                    (abstract-state term2 re2 st2))
+                              (and (equal? term1 term2)
                                    (equal? st1 st2))]))
     ;; astate-hash-code : AState -> Number
     (define astate-hash-code (match-lambda
-                              [(abstract-state node re st) (+ (equal-hash-code node)
+                              [(abstract-state term re st) (+ (equal-hash-code term)
                                                               (equal-hash-code st))]))
     ;; flow-state-similar? : FlowState FlowState -> Boolean
     (define flow-state-similar? (match-lambda*
@@ -121,47 +104,46 @@
     ;; state-equal? : FlowState FlowState -> Boolean
     (define state-equal? equal?)
 
-    ;; succ-node/uid : Key -> [SetOf Key]
-    (define (succ-node/uid uid)
-      (node-graph-get-succs node-graph uid))
-
     ;; succ-states : AState -> [SetOf AState]
     (define succ-states
       (match-lambda
-       [(abstract-state uid env astack)
-        (for/set ([s~ (in-set (succ-node/uid uid))])
-                 (let ((node (uid->node uid)))
-                   (match node
-                     [(push-node pure-rhs)
-                      (abstract-state s~ env (eval-pure-rhs pure-rhs env))]
-                     [(sem-act name params retvars action)
-                      (abstract-state s~ (env-update env
-                                                     (first retvars)
-                                                     (sem-act-val name))
-                                      astack)]
-                     [(go target args)
-                      (match-let (((join-node params) (uid->node s~)))
-                        (abstract-state s~
-                                        (for/fold ([new-env empty-register-env])
-                                                  ([parameter params]
-                                                   [argument args])
-                                          (env-update new-env
-                                                      parameter
-                                                      (eval-pure-rhs argument
-                                                                     env)))
-                                        astack))]
-                     [_ (abstract-state s~ env astack)])))]))
+       [(abstract-state term env astack)
+        (for/set ([s~ (in-set (term-succs term))])
+          (match (term->node term)
+            [(push-node pure-rhs)
+             (abstract-state s~ env (eval-pure-rhs pure-rhs env))]
+            [(sem-act name params retvars action)
+             (abstract-state s~ (env-update env
+                                            (first retvars)
+                                            (sem-act-val name))
+                             astack)]
+            [(go target args)
+             (match-let (((join-point label params) (term->node s~)))
+               (abstract-state s~
+                               (for/fold ([new-env (hash-ref label-closures label)])
+                                         ([parameter params]
+                                          [argument args])
+                                 (env-update new-env
+                                             parameter
+                                             (eval-pure-rhs argument
+                                                            env)))
+                               astack))]
+            [(label ids _ _ _ _ _)
+             (for ((id ids))
+               (hash-set! label-closures id env))
+             (abstract-state s~ env astack)]
+            [_ (abstract-state s~ env astack)]))]))
 
     ;; pop-succ-states : AState AState -> [SetOf AState]
     (define (pop-succ-states push pop)
       (match-define (abstract-state _ _ stack-before-push)
                     push)
-      (match-define (abstract-state pop-uid env stack-after-push)
+      (match-define (abstract-state pop-term env stack-after-push)
                     pop)
 
-      (let ((reg (pop-node-reg (uid->node pop-uid))))
-        (for/set ([node (in-set (succ-node/uid pop-uid))])
-                 (abstract-state node
+      (let ((reg (pop-node-reg (term->node pop-term))))
+        (for/set ([term (in-set (term-succs pop-term))])
+                 (abstract-state term
                                  (env-update env reg stack-after-push)
                                  stack-before-push))))
 
@@ -174,9 +156,9 @@
     ;; astate-join : AbstractState AbstractState -> AbstractState
     (define astate-join
       (match-lambda*
-       [(list (abstract-state node re1 st)
-              (abstract-state node re2 st))
-        (abstract-state node (register-env-join re1 re2) st)]
+       [(list (abstract-state t re1 st)
+              (abstract-state t re2 st))
+        (abstract-state t (register-env-join re1 re2) st)]
        [(list (and a1 (abstract-state _ _ _))
               (and a2 (abstract-state _ _ _)))
         (error 'astate-join
@@ -185,8 +167,8 @@
     ;; astate-gte : AbstractState AbstractState -> Boolean
     (define astate-gte
       (match-lambda*
-       [(list (abstract-state node re1 st)
-              (abstract-state node re2 st))
+       [(list (abstract-state t re1 st)
+              (abstract-state t re2 st))
         (equal? re1 (register-env-join re1 re2))]
        [(list (abstract-state _ _ _)
               (abstract-state _ _ _))
@@ -227,7 +209,7 @@
       (for/set ([astate~ (in-set (pop-succ-states push-astate pop-astate))])
                (flow-state astate~ (max push-fv (next-flow pop-fstate)))))
 
-    (FlowAnalysis (flow-state (abstract-state (node-graph-source node-graph)
+    (FlowAnalysis (flow-state (abstract-state (pda-risc-enh-initial-term pda-risc-enh)
                                               empty-register-env
                                               'ε)
                               5)
@@ -237,8 +219,8 @@
 
   (list (bpset->fv-hash (get-basic-set (time/named
                                         "cfa2 min-headroom analysis"
-                                        (CFA2 (min-headroom node-graph) #:debug debug)))
+                                        (CFA2 (min-headroom) #:debug debug)))
                         (match-lambda [(flow-state as fv) (values as fv)])
                         min
                         +inf.0)
-        node-graph))
+        pda-risc-enh))
