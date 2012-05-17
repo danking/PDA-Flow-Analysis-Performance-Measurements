19f711ddf438244e64a18c3d65e8137c72a2aa93
Daniel King <danking@ccs.neu.edu>
2012-05-17 19:40:57 -0400

    move all the abstract semantics to a separate file

--- run-cfa2.rkt
+++ run-cfa2.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (require "../cfa2/cfa2.rkt"
+         "../semantics/abstract.rkt"
          (only-in "../cfa2/utilities.rkt" bpset->fv-hash)
          (only-in "../racket-utils/similar-sets.rkt" get-basic-set)
          (rename-in "../pda-to-pda-risc/risc-enhanced/data.rkt"
@@ -8,9 +9,7 @@
                     (push push-node)
                     (push? push-node?)
                     (pop pop-var-rhs)
-                    (pop? pop-var-rhs?)
-                    (pda-term-succs term-succs)
-                    (pda-term-insn  term->node)))
+                    (pop? pop-var-rhs?)))
 
 (provide run-cfa2)
 
@@ -20,58 +19,24 @@
      (begin (displayln name)
             (time expr)))))
 
-(define (run-cfa2 pda-risc-enh #:debug [debug 0] #:max-stack-height [max-stack-height +inf.0])
-  ;; an AValue is a [SetOf Value]
-  ;; a RegisterEnv is a [Hash Symbol AValue]
-  (define empty-register-env (hash))
-  ;; an AStack is an AValue
-  ;; an AState is a (make-abstract-state Key RegisterEnv AStack)
-  (define-struct abstract-state (node re st) #:transparent)
+(define (run-cfa2 pda-risc-enh
+                  #:debug [debug 0]
+                  #:max-stack-height [max-stack-height +inf.0])
   ;; a FlowValue is [U PositiveInteger +Infinity]
   ;; a FlowState is a (make-flow-state AState FlowValue)
   (define-struct flow-state (astate flow) #:transparent)
 
-  ;; A SemActVal is how we represent the result of a sem-act
-  (define-struct sem-act-val (name) #:transparent)
-
-
   ;; Min Headroom
   (define (min-headroom)
-    ;; label-closures : LabelName -> RegisterEnv
-    (define label-closures (make-hasheq))
-
-    ;; env-update : RegisterEnv RegisterName [U Value [SetOf Value]] -> RegisterEnv
-    (define (env-update env reg val)
-      (let ((val (if (set? val) val (set val))))
-        (match reg
-          ((register _ uid _ _)
-           (hash-set env uid val)))))
-
-    ;; env-ref : RegisterEnv RegisterName -> [SetOf Value]
-    (define (env-ref env reg)
-      (match reg
-        ((register name uid _ _)
-         (hash-ref env
-                   uid
-                   (lambda () (begin (printf "the register, ~a (uid: ~a), is not bound yet in ~a. Returning 'bottom.\n" name uid env)
-                                     'bottom))))))
-
-    ;; eval-pure-rhs : pure-rhs RegisterEnv -> [SetOf Value]
-    (define (eval-pure-rhs pure-rhs env)
-      (match pure-rhs
-        ((register _ _ _ _)
-         (env-ref env pure-rhs))
-        (_ (set pure-rhs))))
-
     ;; pop-node? : Node -> Boolean
     (define (pop-node? node)
-      (match node
+      (match (pda-term-insn node)
         ((assign-node reg var-rhs)
          (pop-var-rhs? var-rhs))
         (_ #f)))
     ;; pop-node-reg : Node -> RegName
     (define (pop-node-reg pop)
-      (match pop
+      (match (pda-term-insn pop)
         ((assign-node reg var-rhs)
          reg)
         (_ (error 'pop-node-reg "wasn't given a 'pop' node (an assign node)"))))
@@ -85,19 +50,19 @@
 
     ;; astate-similar? : AState AState -> Boolean
     (define astate-similar? (match-lambda*
-                             [(list (abstract-state term1 re1 st1)
-                                    (abstract-state term2 re2 st2))
+                             [(list (abstract-state term1 in1 st1 tr1 re1 le1)
+                                    (abstract-state term2 in2 st2 tr2 re2 le2))
                               (and (eq? term1 term2)
                                    (equal? st1 st2))]))
     ;; astate-hash-code : AState -> Number
     (define astate-hash-code (match-lambda
-                              [(abstract-state term re st)
+                              [(abstract-state term in st tr re le)
                                (+ (equal-hash-code term)
                                   (equal-hash-code st))]))
     ;; astate-equal? : AState AState -> Boolean
     (define astate-equal? (match-lambda*
-                            [(list (abstract-state term1 re1 st1)
-                                   (abstract-state term2 re2 st2))
+                            [(list (abstract-state term1 in1 st1 tr1 re1 le1)
+                                   (abstract-state term2 in2 st2 tr2 re2 le2))
                              (and (eq? term1 term2)
                                   (equal? re1 re2)
                                   (equal? st1 st2))]))
@@ -116,47 +81,11 @@
                             (astate-equal? s1 s2)]))
 
     ;; succ-states : AState -> [SetOf AState]
-    (define succ-states
-      (match-lambda
-       [(abstract-state term env astack)
-        (for/seteq ([s~ (in-set (term-succs term))])
-          (match (term->node term)
-            [(push-node pure-rhs)
-             (abstract-state s~ env (eval-pure-rhs pure-rhs env))]
-            [(sem-act name params retvars action)
-             (abstract-state s~ (env-update env
-                                            (first retvars)
-                                            (sem-act-val name))
-                             astack)]
-            [(go target args)
-             (match-let (((join-point label params) (term->node s~)))
-               (abstract-state s~
-                               (for/fold ([new-env (hash-ref label-closures label)])
-                                         ([parameter params]
-                                          [argument args])
-                                 (env-update new-env
-                                             parameter
-                                             (eval-pure-rhs argument
-                                                            env)))
-                               astack))]
-            [(label ids _ _ _ _ _)
-             (for ((id ids))
-               (hash-set! label-closures id env))
-             (abstract-state s~ env astack)]
-            [_ (abstract-state s~ env astack)]))]))
+    (define succ-states abstract-step)
 
     ;; pop-succ-states : AState AState -> [SetOf AState]
     (define (pop-succ-states push pop)
-      (match-define (abstract-state _ _ stack-before-push)
-                    push)
-      (match-define (abstract-state pop-term env stack-after-push)
-                    pop)
-
-      (let ((reg (pop-node-reg (term->node pop-term))))
-        (for/seteq ([term (in-set (term-succs pop-term))])
-                   (abstract-state term
-                                   (env-update env reg stack-after-push)
-                                   stack-before-push))))
+      (abstract-step/new-stack pop (abstract-state-st push)))
 
     ;; register-env-join : RegisterEnv RegisterEnv -> RegisterEnv
     (define (register-env-join re1 re2)
@@ -167,22 +96,36 @@
     ;; astate-join : AbstractState AbstractState -> AbstractState
     (define astate-join
       (match-lambda*
-       [(list (abstract-state t re1 st)
-              (abstract-state t re2 st))
-        (abstract-state t (register-env-join re1 re2) st)]
-       [(list (and a1 (abstract-state _ _ _))
-              (and a2 (abstract-state _ _ _)))
+       [(list (abstract-state t in st tr1 re1 le)
+              (abstract-state t in st tr2 re2 le))
+        (abstract-state t
+                        in
+                        st
+                        (set-union tr1 tr2)
+                        (register-env-join re1 re2)
+                        le)]
+       [(list (and a1 (abstract-state t1 in1 st1 tr1 re1 le1))
+              (and a2 (abstract-state t2 in2 st2 tr2 re2 le2)))
+        (printf "~v ~v ~v ~v ~v ~v"
+                (equal? t1 t2)
+                (equal? in1 in2)
+                (equal? st1 st2)
+                (equal? tr1 tr2)
+                (equal? re1 re2)
+                (equal? le1 le2))
         (error 'astate-join
-               "States must have matching nodes and stacks. Given ~a and ~a."
+               (string-append "States must have matching nodes, stacks, "
+                              "and label environment. Given ~v and ~v.")
                a1 a2)]))
     ;; astate-gte : AbstractState AbstractState -> Boolean
     (define astate-gte
       (match-lambda*
-       [(list (abstract-state t re1 st)
-              (abstract-state t re2 st))
-        (equal? re1 (register-env-join re1 re2))]
-       [(list (abstract-state _ _ _)
-              (abstract-state _ _ _))
+       [(list (abstract-state t in st tr1 re1 le)
+              (abstract-state t in st tr2 re2 le))
+        (and (equal? re1 (register-env-join re1 re2))
+             (equal? re1 (set-union tr1 tr2)))]
+       [(list (abstract-state _ _ _ _ _ _)
+              (abstract-state _ _ _ _ _ _))
         #f]))
 
     ;; flow-state-join : FlowState FlowState -> FlowState
@@ -209,12 +152,8 @@
     (define (succ-states/flow fstate)
       (match-define (flow-state astate fv) fstate)
 
-      (for/seteq ([astate~ (in-set (succ-states astate))]
-                  [i (set-count (succ-states astate))])
-                 (when (> debug 0)
-                   (printf "[succ-state/flow] ~a\n" i)
-                   (flush-output))
-                 (flow-state astate~ (next-flow fstate))))
+      (for/seteq ([astate~ (in-set (succ-states astate))])
+        (flow-state astate~ (next-flow fstate))))
 
     ;; pop-succ-states/flow : FlowState FlowState -> [SetOf FlowState]
     (define (pop-succ-states/flow push-fstate pop-fstate)
@@ -222,11 +161,9 @@
       (match-define (flow-state pop-astate _) pop-fstate)
 
       (for/seteq ([astate~ (in-set (pop-succ-states push-astate pop-astate))])
-                 (flow-state astate~ (max push-fv (next-flow pop-fstate)))))
+        (flow-state astate~ (max push-fv (next-flow pop-fstate)))))
 
-    (FlowAnalysis (flow-state (abstract-state (pda-risc-enh-initial-term pda-risc-enh)
-                                              empty-register-env
-                                              'ε)
+    (FlowAnalysis (flow-state (init-astate (pda-risc-enh-initial-term pda-risc-enh))
                               5)
                   push-state? pop-state? state-equal?
                   flow-state-join flow-state-gte flow-state-similar? state-hash-code
@@ -238,4 +175,4 @@
                         (match-lambda [(flow-state as fv) (values as fv)])
                         min
                         +inf.0)
-        pda-risc-enh))
+        (unparse pda-risc-enh)))
