82290b85aabc8661a5ecdcb60340bd3d0cee424b
Daniel King <danking@ccs.neu.edu>
2012-06-22 19:27:19 -0400

    slightly more useful output from c/run-cfa2.rkt

--- c/run-cfa2.rkt
+++ c/run-cfa2.rkt
@@ -5,4 +5,10 @@
 
 (file-stream-buffer-mode (current-output-port) 'none)
 
-(pretty-print (run-cfa2 ansi-c-pda-risc))
+; (pretty-print (run-cfa2 ansi-c-pda-risc))
+(match-define (list fv-set sum call pre) (run-cfa2 ansi-c-pda-risc))
+
+(require "../../pda-to-pda-risc/risc-enhanced/data.rkt"
+         "../../pda-to-pda-risc/risc-enhanced/decorate.rkt")
+
+(define pre2 (decorate ansi-c-pda-risc))
